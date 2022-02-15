20fed0653d7f1254f4466b8c06f22bea8f5d212d
efikas <latyf01@gmail.com>
2022-02-15 03:18:13 +0100

    token minting code added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,11 @@
 const Migrations = artifacts.require("Migrations");
+const Market = artifacts.require("Market");
+const NFT = artifacts.require("NFT");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(NFT);
+  deployer.deploy(Market);
 };
+
+// export NODE_OPTIONS=--openssl-legacy-provider
